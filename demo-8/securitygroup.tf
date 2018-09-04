f7c60b480bf98cc740d2c467457595507fb2cc21
AJBenson0702 <40676300+AJBenson0702@users.noreply.github.com>
2018-09-04 11:03:30 -0400

    remove public

--- demo-8/securitygroup.tf
+++ demo-8/securitygroup.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow-ssh" {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["76.117.159.199/32"]
+      cidr_blocks = [""]
   }
 
   ingress {
